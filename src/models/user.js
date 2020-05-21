13be2eeeb7629c8d88e0165258f2902165cc707c
nealthom <nealthom@gmail.com>
2020-05-21 00:00:54 -0500

    Update user routes

--- src/models/user.js
+++ src/models/user.js
@@ -28,9 +28,9 @@ const userSchema = new mongoose.Schema(
             minlength: 7,
             trim: true,
             validate(value){
-                if(value.toLowerCase().includes('password'){
+                if(value.toLowerCase().includes('password')){
                     throw new Error('password cannot contain password')
-                })
+                }
             }
         },
         tokens: [
@@ -69,6 +69,40 @@ userSchema.methods.toJSON = function(){
 
     return userObject
 }
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error("Unable to login");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error("Unable to login");
+  }
+
+  return user;
+};
+
+// hash plain text password before saving
+userSchema.pre("save", async function(next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
+});
+
+userSchema.pre("remove", async function(next) {
+  const user = this;
+  await Task.deleteMany({ owner: user._id });
+
+  next();
+});
 
 
 const User = mongoose.model('User',userSchema)
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,11 +1,38 @@
 const express = require('express')
 const router = new express.Router()
 
+const User = require('../models/user')
 const auth = require('../middleware/auth')
 
 
-router.get('/users',(req,res)=>{
-    res.status(400).send({msg:'Getting started'})
+router.post('/users',async (req,res)=>{
+    try {
+        const user = new User(req.body)
+        const token = await user.generateAuthToken()
+
+        await user.save()
+        res.status(201).send({user,token})
+    } catch (error) {
+        res.status(400).send(error)
+    }
 })
+
+router.post('/users/login', async (req,res)=>{
+    try {
+        const {email, password} = req.body
+        const user = await User.findByCredentials(email,password)
+        const token = await user.generateAuthToken()
+        res.send({user,token})
+    } catch (error) {
+        res.status(400).send()
+    }
+})
+
+router.get('/users/me',auth, async (req,res)=>{
+    
+    res.send(req.user)
+})
+
+
 
 module.exports = router
