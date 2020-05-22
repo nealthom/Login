2f764687f849b0d9b015b020e70f3c9819b2407e
nealthom <nealthom@gmail.com>
2020-05-22 00:17:53 -0500

    heroku setup

--- src/index.js
+++ src/index.js
@@ -11,13 +11,17 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname,'client/build')))
 app.use(userRouter)
 
-// sendback React's index.html file
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'))
-})
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
